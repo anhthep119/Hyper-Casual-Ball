abca31f2329882cf3a3b5eeefcfd29da9588261f
anhthep119 <phamtheanhgl2001@gmail.com>
2024-03-05 15:56:29 +0700

    Update Level Spawn.cs

--- Hyper_Casual_Ball/Assets/Stack Ball/Script/Level Spawn.cs
+++ Hyper_Casual_Ball/Assets/Stack Ball/Script/Level Spawn.cs
@@ -19,6 +19,7 @@ public class LevelSpawn : MonoBehaviour
             addOn = 0;
         
         modelSelection();
+        float random = Random.value;
         for (i = 0; i> -level - addOn; i -= 0.5f)
         {
             if(level <= 20) 
@@ -33,8 +34,20 @@ public class LevelSpawn : MonoBehaviour
             temp1.transform.position = new Vector3(0, i -0.01f,0);
             temp1.transform.eulerAngles = new Vector3(0, i * 8, 0);    
 
+            if(Mathf.Abs(i) >= level * .3f && Mathf.Abs(i) <= level * .6f)
+            {
+                temp1.transform.eulerAngles = new Vector3(0, i * 8, 0);
+                temp1.transform.eulerAngles += Vector3.up * 180;
+            }else if(Mathf.Abs(i) >= level * .8f)
+            {
+                temp1.transform.eulerAngles = new Vector3(0, i * 8, 0);
+                if (random > .75f)
+                    temp1.transform.eulerAngles += Vector3.up * 180;
+            }
+
             temp1.transform.parent = FindObjectOfType<Rotator>().transform;
         }
+
         temp2 = Instantiate(WinPrefab);
         temp2.transform.position = new Vector3(0, i - 0.01f, 0);
     }
